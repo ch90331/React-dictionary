4e6d571b985cd1a5e6e1cb257585ef30f2f56353
ch90331 <62222044+ch90331@users.noreply.github.com>
2021-05-15 23:25:24 +0100

    Added alt on the image element

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="img-fluid App-logo shadow"/>
+        <img src={logo} className="img-fluid App-logo shadow" alt="logo"/>
         <span>
           Explore the <strong>MEANING</strong> of life
         </span>
