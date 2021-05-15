dd3ec09c23c01230ddc081b6e55aa33c1d565ae4
ch90331 <62222044+ch90331@users.noreply.github.com>
2021-05-15 22:57:50 +0100

    Initial set up for the app

---
+++ react-dictionary/src/App.js
@@ -0,0 +1,16 @@
+import logo from './feet.jpeg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="img-fluid App-logo shadow"/>
+        <span>
+          Explore the <strong>MEANING</strong> of life
+        </span>
+      </header>
+    </div>
+  );
+}
+
