d17cdfc9dd6fe79e1e9cf64f7b214079a33784a1
ch90331 <62222044+ch90331@users.noreply.github.com>
2021-06-20 13:12:22 +0100

    Added api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,23 @@
+import axios from "axios";
 import React, {useState} from "react";
 import './Dictionary.css';
 
 export default function Dictionary() {
   let [keyword,setKeyword]=useState("");
 
+  function displayResponse(response){
+   console.log(response.data[0]);
+  }
+
   function search (event){
     event.preventDefault();
     alert(`Explore the meaning of ${keyword}`);
-  }
 
+    let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayResponse);
+  }
+  
   function showKeyword (event){
     setKeyword(event.target.value);
   }
