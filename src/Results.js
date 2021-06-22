f7cf879064e6d7a43511eaa1e7215a39b794c4f1
ch90331 <62222044+ch90331@users.noreply.github.com>
2021-06-23 00:33:40 +0100

    Tried to test "if statement" in the component of Results but still not working

--- src/Results.js
+++ src/Results.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export default function Results(props){
     console.log(props.results);
-    return <div className="Results">
+    if (props.results){
+     return <div className="Results">
     {props.results.word}
-    </div>;
+    </div>;} 
+     return null;
     }
